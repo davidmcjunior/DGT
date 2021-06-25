5df8f7856b9a44325f556d9d37d5a54111ed2054
David McClure Humphrey <5810616-davidmhjr@users.noreply.gitlab.com>
2021-06-24 21:30:26 -0400

    Move declarations to different module.

--- DGT/ClientApp/src/app/components/editor/editor.module.ts
+++ DGT/ClientApp/src/app/components/editor/editor.module.ts
@@ -4,7 +4,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { EditorComponent } from './editor/editor.component';
 import { MapComponent } from './editor/map/map.component';
 import { ReadonlyAttributesComponent } from './editor/attributes/readonly-attributes.component';
-import { CrashEventRecordComponent } from './editor/crash-event-record/crash-event-record.component';
 import { StatusBarComponent } from './editor/status-bar/status-bar.component';
 import { ButtonsBarComponent } from './editor/buttons-bar/buttons-bar.component';
 import { EditorRoutingModule } from './editor-routing.module';
@@ -15,28 +14,12 @@ import { AutocompleteLibModule } from 'angular-ng-autocomplete';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { SiteLocationComponent } from "app/components/editor/editor/crash-event-record/crash-event-record-controls/site-location.component";
-import { CrashInfoPanelComponent } from "app/components/editor/editor/crash-event-record/crash-event-record-controls/crash-info-panel.component";
-import { CrashInjuryComponent } from "app/components/editor/editor/crash-event-record/crash-event-record-controls/crash-injury.component";
-import { CrashLaneComponent } from "app/components/editor/editor/crash-event-record/crash-event-record-controls/crash-lane.component";
-import { DotPropertyComponent } from "app/components/editor/editor/crash-event-record/crash-event-record-controls/dot-property.component";
-import { OnPublicRoadsComponent } from "app/components/editor/editor/crash-event-record/crash-event-record-controls/on-public-roads.component";
-import { RoadwaySystemIdComponent } from "app/components/editor/editor/crash-event-record/crash-event-record-controls/roadway-system-id.component";
-import { SideOfRoadComponent } from "app/components/editor/editor/crash-event-record/crash-event-record-controls/side-of-road.component";
-import { BicyclistCountComponent } from "app/components/editor/editor/crash-event-record/crash-event-record-controls/bicyclist-count.component";
-import { FunctionalClassComponent } from "app/components/editor/editor/crash-event-record/crash-event-record-controls/functional-class.component";
-import { NumberOfLanesComponent } from "app/components/editor/editor/crash-event-record/crash-event-record-controls/number-of-lanes.component";
-import { OwnershipComponent } from "app/components/editor/editor/crash-event-record/crash-event-record-controls/ownership.component";
-import { PedestrianCountComponent } from "app/components/editor/editor/crash-event-record/crash-event-record-controls/pedestrian-count.component";
-import { PostedSpeedLimitComponent } from "app/components/editor/editor/crash-event-record/crash-event-record-controls/posted-speed-limit.component";
-import { RouteSignageComponent } from "app/components/editor/editor/crash-event-record/crash-event-record-controls/route-signage.component";
+import { CrashEventModule } from "./editor/crash-event-record/crash-event-record.module";
 
 
 @NgModule({
   declarations: [
     EditorComponent,
-    MapComponent,
-    CrashEventRecordComponent,
     ReadonlyAttributesComponent,
     MapComponent,
     StatusBarComponent,
@@ -44,21 +27,6 @@ import { RouteSignageComponent } from "app/components/editor/editor/crash-event-
     TimePipe,
     GiveUpDialogComponent,
     SearchComponent,
-    SiteLocationComponent,
-    CrashInfoPanelComponent,
-    CrashInjuryComponent,
-    CrashLaneComponent,
-    DotPropertyComponent,
-    OnPublicRoadsComponent,
-    RoadwaySystemIdComponent,
-    SideOfRoadComponent,
-    BicyclistCountComponent,
-    FunctionalClassComponent,
-    NumberOfLanesComponent,
-    OwnershipComponent,
-    PedestrianCountComponent,
-    PostedSpeedLimitComponent,
-    RouteSignageComponent
   ],
   imports: [
     CommonModule,
@@ -67,7 +35,8 @@ import { RouteSignageComponent } from "app/components/editor/editor/crash-event-
     AutocompleteLibModule,
     MatAutocompleteModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    CrashEventModule
   ]
 })
 export class EditorModule {}
---
+++ DGT/ClientApp/src/app/components/editor/editor/crash-event-record/crash-event-record.module.ts
@@ -0,0 +1,43 @@
+import {NgModule} from "@angular/core";
+import { CrashEventRecordComponent } from './crash-event-record.component';
+import { SiteLocationComponent } from "./crash-event-record-controls/site-location.component";
+import { CrashInfoPanelComponent } from "./crash-event-record-controls/crash-info-panel.component";
+import { CrashInjuryComponent } from "./crash-event-record-controls/crash-injury.component";
+import { CrashLaneComponent } from "./crash-event-record-controls/crash-lane.component";
+import { DotPropertyComponent } from "./crash-event-record-controls/dot-property.component";
+import { OnPublicRoadsComponent } from "./crash-event-record-controls/on-public-roads.component";
+import { RoadwaySystemIdComponent } from "./crash-event-record-controls/roadway-system-id.component";
+import { SideOfRoadComponent } from "./crash-event-record-controls/side-of-road.component";
+import { BicyclistCountComponent } from "./crash-event-record-controls/bicyclist-count.component";
+import { FunctionalClassComponent } from "./crash-event-record-controls/functional-class.component";
+import { NumberOfLanesComponent } from "./crash-event-record-controls/number-of-lanes.component";
+import { OwnershipComponent } from "./crash-event-record-controls/ownership.component";
+import { PedestrianCountComponent } from "./crash-event-record-controls/pedestrian-count.component";
+import { PostedSpeedLimitComponent } from "./crash-event-record-controls/posted-speed-limit.component";
+import { RouteSignageComponent } from "./crash-event-record-controls/route-signage.component";
+
+const classList = [
+  CrashEventRecordComponent,
+  SiteLocationComponent,
+  CrashInfoPanelComponent,
+  CrashInjuryComponent,
+  CrashLaneComponent,
+  DotPropertyComponent,
+  OnPublicRoadsComponent,
+  RoadwaySystemIdComponent,
+  SideOfRoadComponent,
+  BicyclistCountComponent,
+  FunctionalClassComponent,
+  NumberOfLanesComponent,
+  OwnershipComponent,
+  PedestrianCountComponent,
+  PostedSpeedLimitComponent,
+  RouteSignageComponent
+];
+
+@NgModule({
+  declarations: classList,
+  exports: classList,
+  imports: []
+})
+export class CrashEventModule {}
