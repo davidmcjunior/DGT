9c833869f64281364cf4add92f3bdfc7b976d087
mtclark <mtclark@geoplan.ufl.edu>
2021-07-27 20:50:58 -0400

    adds shared module to app module for tooltip settings

--- DGT/ClientApp/src/app/app.component.ts
+++ DGT/ClientApp/src/app/app.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { TooltipSettings } from '@progress/kendo-angular-tooltip';
+import { TOOLTIP_SETTINGS } from './models/constants';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  providers: [{
+    provide: TooltipSettings,
+    useFactory: TOOLTIP_SETTINGS
+}]
 })
 export class AppComponent {
   title = 'dgt-frontend';
