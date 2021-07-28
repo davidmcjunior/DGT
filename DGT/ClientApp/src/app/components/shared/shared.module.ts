db4187430ecd8a5158135bfb8312d3ddc7722d63
mtclark <mtclark@geoplan.ufl.edu>
2021-07-27 20:48:03 -0400

    add icon button component

--- DGT/ClientApp/src/app/components/shared/shared.module.ts
+++ DGT/ClientApp/src/app/components/shared/shared.module.ts
@@ -2,19 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { LoadingOverlayComponent } from './loading-overlay.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TooltipModule } from '@progress/kendo-angular-tooltip';
+import { IconButtonComponent } from './icon-button/icon-button.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    LoadingOverlayComponent
+    LoadingOverlayComponent,
+    IconButtonComponent
   ],
   exports: [
-    LoadingOverlayComponent
+    TooltipModule,
+    LoadingOverlayComponent,
+    IconButtonComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TooltipModule
   ]
 })
 export class SharedModule { }
