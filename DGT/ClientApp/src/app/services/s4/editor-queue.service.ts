d49cfbccb51d466fb92ee7514560156d822c38e2
David McClure Humphrey <5810616-davidmhjr@users.noreply.gitlab.com>
2021-07-27 20:55:01 -0400

    Hmmm...

--- DGT/ClientApp/src/app/services/s4/editor-queue.service.ts
+++ DGT/ClientApp/src/app/services/s4/editor-queue.service.ts
@@ -1,31 +1,39 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import {WatchableService} from "../../models/services/watchable-service";
+import {WatchableService} from "app/models/services/watchable-service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class EditorQueueService extends WatchableService {
   private _index = 0;
+  private _queue = [
+    100,
+    200,
+    300,
+    400,
+    88416900,
+    24089254,
+    88030750,
+    24089323,
+    24089712,
+    24089722,
+    88472946,
+    89918672,
+    88437234,
+    88437237
+  ];
 
   constructor(private _http: HttpClient) {
     super();
   }
 
-  public getQueue(): Observable<any> {
-    return of([100, 200, 300, 400]);
+  public prev(): number {
+    return this._index > 0 ? this._queue[this._index--] : this._queue[this._index];
   }
 
-  public next() {
-    return this._index += 100;
-  }
-
-  public prev() {
-    if (this._index > 100) {
-      return this._index -= 100;
-    }
-
-    else return 100;
+  public next(): number {
+    return 100;
+    // return this._index < this._queue.length ? this._queue[this._index++] : this._queue[this._index];
   }
 }
